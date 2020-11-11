5fc6d1df548f1b9f4adff79beda19159dd3454d6
rfsants001 <rf.sants001@gmail.com>
2020-11-11 08:56:12 -0300

    Finished front end form

--- Starter/scripts/create-point.js
+++ Starter/scripts/create-point.js
@@ -23,11 +23,14 @@ function getCities(event){
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`;
 
+    citySelect.innerHTML = "<option value>Selecione a Cidade</option>";
+    citySelect.disabled = false;
+
     fetch(url)
         .then( res => res.json())
         .then( cities => {
             for(city of cities) {
-                citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+                citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
             }
 
             citySelect.disabled = false;
@@ -38,3 +41,36 @@ function getCities(event){
 document
     .querySelector("select[name=uf]")
     .addEventListener("change", getCities);
+
+
+// Itens de coleta
+
+const itemsToCollect = document.querySelectorAll(".items-grid li");
+
+for (item of itemsToCollect){
+    item.addEventListener("click", handleSelectedItem);
+}
+
+const collectedItems = document.querySelector("input[name=items]");
+
+let selectedItems = [];
+
+function handleSelectedItem(event){
+    const itemLi = event.target;
+
+    //add or remove class
+    itemLi.classList.toggle("selected");
+    
+    const itemId = itemLi.dataset.id;
+
+    const alreadySelected = selectedItems.findIndex(item => item == itemId); //verifica se existe itens selecionado
+
+    if(alreadySelected >= 0){ //se ja estiver selecionado
+        const filteredItems = selectedItems.filter( item => item != itemId);
+        selectedItems = filteredItems;
+    } else { //se não estiver selecionado
+        selectedItems.push(itemId)
+    }
+
+    collectedItems.value = selectedItems;
+}
