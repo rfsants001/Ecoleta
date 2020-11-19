b33ab65e27ef8703ab7c8ed17c350423045bc129
rfsants001 <rf.sants001@gmail.com>
2020-11-18 22:46:50 -0300

    Config nunjucks

--- Starter/src/server.js
+++ Starter/src/server.js
@@ -1,4 +1,30 @@
 const express = require('express');
 const server = express();
 
+server.use(express.static("public"));
+
+
+
+//template engine
+const nunjucks = require('nunjucks');
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+});
+
+
+
+server.get("/", (req, res) => {
+    return res.render("index.html");
+});
+
+server.get("/create-point", (req, res) => {
+    return res.render("create-point.html");
+});
+
+server.get("/search", (req, res) => {
+    return res.render("search-results.html");
+});
+
+
 server.listen(3000);
