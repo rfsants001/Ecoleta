6e2e5d2907f147d609ce897f2fc9bd2f7530871b
rfsants001 <rf.sants001@gmail.com>
2020-11-21 21:07:04 -0300

    Add DB and functions

--- Starter/src/server.js
+++ Starter/src/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const server = express();
 
+const db = require('./database/db');
+
 server.use(express.static("public"));
+
+server.use(express.urlencoded({ extended: true }));
 
 
 
@@ -13,17 +17,94 @@ nunjucks.configure("src/views", {
 });
 
 
-
+//HOME
 server.get("/", (req, res) => {
     return res.render("index.html");
 });
 
+
+
+
+//CREATE POINT
 server.get("/create-point", (req, res) => {
+
+    
     return res.render("create-point.html");
 });
 
+server.post("/create-point", (req, res) => {
+
+    db.run(`
+        CREATE TABLE IF NOT EXISTS places (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            image TEXT,
+            name,
+            address TEXT,
+            address2 TEXT,
+            state TEXT,
+            city TEXT,
+            items TEXT
+        );
+    `);
+
+
+    const query = `
+        INSERT INTO places (
+            image,
+            name,
+            address,
+            address2,
+            state,
+            city,
+            items
+        ) VALUES (?,?,?,?,?,?,?);
+    `
+
+    const values = [
+            req.body.image,
+            req.body.name,
+            req.body.address,
+            req.body.address2,
+            req.body.state,
+            req.body.city,
+            req.body.items
+        ]
+
+    function afterInsertData(err){
+        if(err){
+            console.log(err);
+            res.send("Erro no cadastro")
+        }
+
+
+        return res.render("create-point.html", { saved: true });
+    }
+
+    db.run(query, values, afterInsertData);
+
+});
+
+
+
+//SEARCH
 server.get("/search", (req, res) => {
-    return res.render("search-results.html");
+
+    const search = req.query.search;
+
+
+
+    //pega os dados do banco
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows){
+        if(err){
+            return console.log(err);
+        }
+
+        const total = rows.length;
+
+
+        return res.render("search-results.html", {places: rows, total, searchText: search});
+    });
+
 });
 
 
